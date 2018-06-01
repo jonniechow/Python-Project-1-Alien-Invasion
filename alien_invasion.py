9a0fd0fa813aa8c140a103b578aae1e64299e151
Jonny Chow <Jonny@MacBook-Pro-8.local>
2018-06-01 13:41:18 -0400

    Allows ship to move left and right based on arrow keys being pressed.

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,13 +17,8 @@ def run_game():
 
     # Start the main loop for the game.
     while True:
-        gf.check_events()
-        
-        # Redraw the screen during each pass through the loop.
-        screen.fill(ai_settings.bg_color)
-        ship.blitme()
-
-        # Make the most recently drawn screen visible.
-        pygame.display.flip()
+        gf.check_events(ship)
+        ship.update()
+        gf.update_screen(ai_settings, screen, ship)      
     
 run_game()

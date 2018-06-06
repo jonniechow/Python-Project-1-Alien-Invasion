5e93aa21c3b4cd7a79eb9f1cb2cbe544b9d02540
Jonny Chow <Jonny@MacBook-Pro-8.local>
2018-06-06 12:59:11 -0400

    Updated alien settings for a smoother experience.

--- alien.py
+++ alien.py
@@ -28,9 +28,9 @@ class Alien(Sprite):
     def check_edges(self):
         """Return True if alien is at edge of screen."""
         screen_rect = self.screen.get_rect()
-        if self.rect.right >= screen_rect.right:
+        if self.rect.right >= screen_rect.right - 150:
             return True
-        elif self.rect.left <= 0:
+        elif self.rect.left <= 50:
             return True
         
     def update(self):
--- game_functions.py
+++ game_functions.py
@@ -131,7 +131,7 @@ def check_bullet_alien_collisions(ai_settings, screen, stats, sb, ship,
 
 def get_number_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit in a row."""
-    available_space_x = ai_settings.screen_width - 2 * alien_width
+    available_space_x = ai_settings.screen_width - 6 * alien_width
     number_aliens_x = int(available_space_x / (2 * alien_width))
     return number_aliens_x
 
@@ -139,7 +139,7 @@ def get_number_rows(ai_settings, ship_height, alien_height):
     """Determine the number of rows of aliens that fit on the screen."""
     available_space_y = (ai_settings.screen_height - 
                             (3 * alien_height) - ship_height)
-    number_rows = int(available_space_y / (2 * alien_height))
+    number_rows = int(available_space_y / (3 * alien_height))
     return number_rows
 
 def create_alien(ai_settings, screen, aliens, alien_number, row_number):
